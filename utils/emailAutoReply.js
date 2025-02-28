5ff540eb75641fa5c46ad4c9886c50c8445474ea
daniel-idowu-01 <danielidowu414@gmail.com>
2025-02-28 10:39:56 +0100

    refactor: update auto-reply logic to use shouldAutoReply function and remove deprecated classification code

--- utils/emailAutoReply.js
+++ utils/emailAutoReply.js
@@ -1,22 +1,6 @@
-import { classifyText } from "../services/classifyEmailService.js";
+import { shouldAutoReply } from "../services/classifyEmailService.js";
 import { getUnreadEmails, sendAutoReply } from "../integrations/gmailService.js";
 import { logAIDecision } from "../usecases/aiDecicionLogs.js";
-
-// =======================
-// Check if email should receive an auto-reply
-// =======================
-export const shouldAutoReply = async (email) => {
-  const classification = await classifyText(email.snippet);
-
-  const autoReplyKeywords = [
-    "out of office",
-    "support",
-    "inquiry",
-    "urgent",
-    "thank you",
-  ];
-  return autoReplyKeywords.some((keyword) => classification.includes(keyword));
-};
 
 // =======================
 // Process unread emails & send auto-replies if needed
